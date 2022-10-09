2c6d18d17544985a5462feda7d150a18dfbc1469
NjordJ <nj0rd3841@gmail.com>
2022-10-09 16:55:54 +0500

    Details screen with data

--- app/src/main/java/com/iruda/simpleimagesearch/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/iruda/simpleimagesearch/ui/details/DetailsFragment.kt
@@ -1,11 +1,22 @@
 package com.iruda.simpleimagesearch.ui.details
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.iruda.simpleimagesearch.R
 import com.iruda.simpleimagesearch.databinding.FragmentDetailsBinding
 import com.iruda.simpleimagesearch.ui.gallery.GalleryViewModel
 
@@ -17,7 +28,8 @@ class DetailsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private val viewModel: GalleryViewModel by viewModels()
+    private val viewModel: DetailsViewModel by viewModels()
+    private val args: DetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +45,52 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            val photo = args.photo
+
+            Glide.with(this@DetailsFragment)
+                .load(photo.urls.regular)
+                .error(R.drawable.ic_error)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        textViewCreator.isVisible = true
+                        textViewDescription.isVisible = photo.description != null
+                        return false
+                    }
+                })
+                .into(imageView)
+
+            textViewDescription.text = photo.description
+
+            val uri = Uri.parse(photo.user.attributionUrl)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            textViewCreator.apply {
+                text = "Photo by ${photo.user.name} on Unsplash"
+                setOnClickListener {
+                    context.startActivity(intent)
+                }
+                paint.isUnderlineText = true
+            }
+        }
     }
 
     override fun onDestroyView() {
